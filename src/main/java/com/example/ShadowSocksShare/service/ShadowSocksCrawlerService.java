54a404238e03100598af6220d1ec6cde71e1b27b
xxx <x@x.x>
2018-01-02 12:52:32 +0800

    修复： 修复 生成 订阅字符串的 BUG
    Signed-off-by: xxx <x@x.x>

--- src/main/java/com/example/ShadowSocksShare/service/ShadowSocksCrawlerService.java
+++ src/main/java/com/example/ShadowSocksShare/service/ShadowSocksCrawlerService.java
@@ -101,7 +101,7 @@ public abstract class ShadowSocksCrawlerService {
 
 				String[] ssInfo = StringUtils.split(strs[0], ":", 6);
 
-				ShadowSocksDetailsEntity entity = new ShadowSocksDetailsEntity(ssInfo[0], Integer.parseInt(ssInfo[1]), new String(Base64.decodeBase64(ssInfo[5])), ssInfo[3], ssInfo[2], ssInfo[4]);
+				ShadowSocksDetailsEntity entity = new ShadowSocksDetailsEntity(ssInfo[0].trim(), Integer.parseInt(ssInfo[1].trim()), new String(Base64.decodeBase64(ssInfo[5].trim())), ssInfo[3].trim(), ssInfo[2].trim(), ssInfo[4].trim());
 
 				/*String suffix_base64 = strs[1];
 				byte[] _remarks = Base64.decodeBase64(StringUtils.substringBetween(suffix_base64, "remarks=", "&"));
@@ -122,7 +122,7 @@ public abstract class ShadowSocksCrawlerService {
 			String[] ssInfo1 = StringUtils.split(strs[0], ":");
 			String[] ssInfo2 = StringUtils.split(strs[1], ":");
 
-			ShadowSocksDetailsEntity entity = new ShadowSocksDetailsEntity(ssInfo2[0], Integer.parseInt(ssInfo2[1].trim()), ssInfo1[1], ssInfo1[0], "origin", "plain");
+			ShadowSocksDetailsEntity entity = new ShadowSocksDetailsEntity(ssInfo2[0].trim(), Integer.parseInt(ssInfo2[1].trim()), ssInfo1[1].trim(), ssInfo1[0].trim(), "origin", "plain");
 			return entity;
 		} else {
 			throw new IllegalArgumentException("SSR 连接[" + link + "]解析异常：协议类型错误");
