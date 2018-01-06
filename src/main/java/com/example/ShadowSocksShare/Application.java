1aa3ad36acda60b9975509bb385c760459298657
xxx <x@x.x>
2018-01-06 14:40:56 +0800

    系统优化
    Signed-off-by: xxx <x@x.x>

--- src/main/java/com/example/ShadowSocksShare/Application.java
+++ src/main/java/com/example/ShadowSocksShare/Application.java
@@ -2,6 +2,7 @@ package com.example.ShadowSocksShare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
@@ -9,6 +10,7 @@ import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class Application {
 
 	/**
